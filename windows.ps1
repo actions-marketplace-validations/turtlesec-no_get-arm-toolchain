a729b1f3f015edf8a7bc4eb59166650d58800242
Patricia Aas <psmaas@gmail.com>
2021-06-30 22:37:53 +0200

    Try to set the path from the powershell script

--- windows.ps1
+++ windows.ps1
@@ -2,4 +2,5 @@ Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.
 scoop bucket add extras
 scoop install gcc-arm-none-eabi
 echo "$HOME/scoop/apps/gcc-arm-none-eabi/10-2020-q4-major/bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+echo "INSTALL_DIR=$HOME/scoop/apps/gcc-arm-none-eabi/10-2020-q4-major/bin" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf-8 -Append
 arm-none-eabi-gcc --version
