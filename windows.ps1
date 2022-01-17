b86db33876732bc3ae8b81d05ec78d8258e79e3a
Patricia Aas <patricia@turtlesec.no>
2022-01-17 16:05:24 +0100

    Update toolchain version

--- windows.ps1
+++ windows.ps1
@@ -2,9 +2,9 @@ Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.
 scoop bucket add extras
 scoop install gcc-arm-none-eabi
 scoop info gcc-arm-none-eabi
-echo "$HOME/scoop/apps/gcc-arm-none-eabi/10-2020-q4-major/bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+echo "$HOME/scoop/apps/gcc-arm-none-eabi/10.3-2021.10/bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
 $pattern = '[\\/]'
-$string = "$HOME/scoop/apps/gcc-arm-none-eabi/10-2020-q4-major/bin"
+$string = "$HOME/scoop/apps/gcc-arm-none-eabi/10.3-2021.10/bin"
 $string = $string -replace $pattern, '/'
 echo "INSTALL_DIR=$string" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
 arm-none-eabi-gcc --version
