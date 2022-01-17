05d4dd80ec51f60962c646b7dffbe94a50527813
Patricia Aas <patricia@turtlesec.no>
2022-01-17 15:58:23 +0100

    Add scoop info

--- windows.ps1
+++ windows.ps1
@@ -1,6 +1,7 @@
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 scoop bucket add extras
 scoop install gcc-arm-none-eabi
+scoop info gcc-arm-none-eabi
 echo "$HOME/scoop/apps/gcc-arm-none-eabi/10-2020-q4-major/bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
 $pattern = '[\\/]'
 $string = "$HOME/scoop/apps/gcc-arm-none-eabi/10-2020-q4-major/bin"
