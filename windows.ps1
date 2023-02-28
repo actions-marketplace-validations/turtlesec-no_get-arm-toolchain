e2a14b133dd58820df1db798a695f63f95abd7f1
Patricia Aas <patricia@turtlesec.no>
2023-02-28 15:32:25 +0100

    Preserve behavior regarding slashes on Windows

--- windows.ps1
+++ windows.ps1
@@ -7,8 +7,10 @@ $arm_exe_path = scoop which arm-none-eabi-gcc
 $arm_path = [System.IO.Path]::GetDirectoryName($arm_exe_path)
 $arm_path_resolved = Resolve-Path($arm_path) | select -ExpandProperty Path
 echo "$arm_path_resolved" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
-echo "Setting INSTALL_DIR to: $arm_path_resolved"
-echo "INSTALL_DIR=$arm_path_resolved" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+$pattern = '[\\/]'
+$install_dir = $arm_path_resolved -replace $pattern, '/'
+echo "Setting INSTALL_DIR to: $install_dir"
+echo "INSTALL_DIR=$install_dir" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
 
 $version = arm-none-eabi-gcc --version | select -first 1
 echo "Setting VERSION to: $version"
