b04e5685625fd929fcb9f45f985d88779fa654c9
Patricia Aas <patricia@turtlesec.no>
2023-02-28 14:58:53 +0100

    Clean up PowerShell

--- windows.ps1
+++ windows.ps1
@@ -3,16 +3,13 @@ scoop bucket add extras
 scoop install gcc-arm-none-eabi
 scoop info gcc-arm-none-eabi
 
-$path = [System.IO.Path];
 $arm_exe_path = scoop which arm-none-eabi-gcc
-$arm_path = $path::GetDirectoryName($arm_exe_path)
+$arm_path = [System.IO.Path]::GetDirectoryName($arm_exe_path)
 $arm_path_resolved = Resolve-Path($arm_path) | select -ExpandProperty Path
 echo "$arm_path_resolved" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
-# $pattern = '[\\/]'
-# $string = "$HOME/scoop/apps/gcc-arm-none-eabi/10.3-2021.10/bin"
-# $string = $string -replace $pattern, '/'
+echo "Setting INSTALL_DIR to: $arm_path_resolved"
 echo "INSTALL_DIR=$arm_path_resolved" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
 
 $version = arm-none-eabi-gcc --version | select -first 1
-echo $version
+echo "Setting VERSION to: $version"
 echo "VERSION=$version" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
