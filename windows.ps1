fc8ef8cc988d2ff9814bf5c4f96de27c85944aea
Patricia Aas <patricia@turtlesec.no>
2023-02-28 14:15:14 +0100

    Use powershell and scoop to get the path

--- windows.ps1
+++ windows.ps1
@@ -3,11 +3,15 @@ scoop bucket add extras
 scoop install gcc-arm-none-eabi
 scoop info gcc-arm-none-eabi
 
-echo "$HOME/scoop/apps/gcc-arm-none-eabi/10.3-2021.10/bin" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
-$pattern = '[\\/]'
-$string = "$HOME/scoop/apps/gcc-arm-none-eabi/10.3-2021.10/bin"
-$string = $string -replace $pattern, '/'
-echo "INSTALL_DIR=$string" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+$path = [System.IO.Path];
+$arm_exe_path = scoop which arm-none-eabi-gcc
+$arm_path = $path::GetDirectoryName($arm_exe_path)
+$arm_path_resolved = Resolve-Path($arm_path) | select -ExpandProperty Path
+echo "$arm_path_resolved" | Out-File -FilePath $env:GITHUB_PATH -Encoding utf8 -Append
+# $pattern = '[\\/]'
+# $string = "$HOME/scoop/apps/gcc-arm-none-eabi/10.3-2021.10/bin"
+# $string = $string -replace $pattern, '/'
+echo "INSTALL_DIR=$arm_path_resolved" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
 
 $version = arm-none-eabi-gcc --version | select -first 1
 echo $version
